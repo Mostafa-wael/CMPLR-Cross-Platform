f9f048f0ea5cacfdd866fe935b1dfb76a23b171c
Mostafa Elgendy <62804966+mostafa-elgendy22@users.noreply.github.com>
2021-11-12 21:45:53 +0200

    Preferences screen dialog

--- cmplr/lib/views/android_views/signup_preferences.dart
+++ cmplr/lib/views/android_views/signup_preferences.dart
@@ -8,87 +8,144 @@ class SignupPreferences extends StatelessWidget {
   const SignupPreferences({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<SignupPreferencesController>(
-      init: SignupPreferencesController(),
-      builder: (controller) => Scaffold(
-          appBar: AppBar(
-            shadowColor: Colors.transparent,
-            backgroundColor: const Color(0xFF001A35),
-            actions: [
-              (controller.isLoading)
-                  ? CollectionScaleTransition(
+    Future<bool> _onBackPressed() async {
+      return await showDialog(
+          context: context,
+          builder: (context) => Dialog(
+              insetPadding: EdgeInsets.zero,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              backgroundColor: Colors.white,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 145,
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Text(
+                          'Aw. If you leave now, you\'ll lose all your progress.',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 21)),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
                       children: [
-                        Icon(Icons.crop_square_outlined),
-                        Icon(Icons.crop_square_outlined),
-                        Icon(Icons.crop_square_outlined),
+                        SizedBox(width: 200),
+                        TextButton(
+                            onPressed: () {
+                              Get.back();
+                            },
+                            child: Text(
+                              'CONTINUE',
+                              style: TextStyle(fontSize: 16),
+                            )),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              // go to main screen here
+                            },
+                            child:
+                                Text('LEAVE', style: TextStyle(fontSize: 16)))
                       ],
                     )
-                  : Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        highlightColor: Colors.transparent,
-                        splashColor: Color(0x33f2f2f2),
-                        onTap: () {
-                          controller.nextButtonPressed();
-                        },
-                        child: Center(
-                          child: Text(
-                            '  ${controller.buttonText}  ',
-                            style: TextStyle(
-                                color: Color(controller.buttonTextColor),
-                                fontSize: 20,
-                                fontWeight: FontWeight.w800),
+                  ],
+                ),
+              )));
+    }
+
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: GetBuilder<SignupPreferencesController>(
+        init: SignupPreferencesController(),
+        builder: (controller) => Scaffold(
+            appBar: AppBar(
+              shadowColor: Colors.transparent,
+              backgroundColor: const Color(0xFF001A35),
+              actions: [
+                (controller.isLoading)
+                    ? CollectionScaleTransition(
+                        children: [
+                          Icon(Icons.crop_square_outlined),
+                          Icon(Icons.crop_square_outlined),
+                          Icon(Icons.crop_square_outlined),
+                        ],
+                      )
+                    : Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          highlightColor: Colors.transparent,
+                          splashColor: Color(0x33f2f2f2),
+                          onTap: () {
+                            controller.nextButtonPressed();
+                          },
+                          child: Center(
+                            child: Text(
+                              '  ${controller.buttonText}  ',
+                              style: TextStyle(
+                                  color: Color(controller.buttonTextColor),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w800),
+                            ),
                           ),
-                        ),
-                      ))
-            ],
-          ),
-          body: Column(children: [
-            Container(
-              width: double.infinity,
-              height: 115,
-              color: Color(0xFF001A35),
-              padding: EdgeInsets.fromLTRB(15, 4, 15, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'What do you like?',
-                    style: TextStyle(color: Colors.white, fontSize: 30),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Whatever you\'re into, you\'ll find it here. '
-                    'Follow some of the tags below to start filling '
-                    'your dashboard with the things you love.',
-                    style: TextStyle(color: Color(0xFF7F8D9C), fontSize: 19),
-                  ),
-                ],
-              ),
+                        ))
+              ],
             ),
-            Expanded(
-                child: Container(
-                    padding: const EdgeInsets.fromLTRB(10, 6, 10, 0),
-                    color: const Color(0xFF001A35),
-                    child: GridView.builder(
-                        primary: false,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            childAspectRatio:
-                                (MediaQuery.of(context).size.width / 3) / 150,
-                            crossAxisCount: 3,
-                            crossAxisSpacing: 12,
-                            mainAxisSpacing: 2),
-                        itemCount:
-                            controller.availablePreferenceCards.length + 1,
-                        itemBuilder: (context, index) {
-                          if (index == 0) {
-                            return buildCustomCard(controller);
-                          }
-                          return buildPreferencesCard(controller, index - 1);
-                        }))),
-          ])),
+            body: Column(children: [
+              Container(
+                width: double.infinity,
+                height: 115,
+                color: Color(0xFF001A35),
+                padding: EdgeInsets.fromLTRB(15, 4, 15, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'What do you like?',
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Whatever you\'re into, you\'ll find it here. '
+                      'Follow some of the tags below to start filling '
+                      'your dashboard with the things you love.',
+                      style: TextStyle(color: Color(0xFF7F8D9C), fontSize: 19),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                  child: Container(
+                      padding: const EdgeInsets.fromLTRB(10, 6, 10, 0),
+                      color: const Color(0xFF001A35),
+                      child: GridView.builder(
+                          primary: false,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  childAspectRatio:
+                                      (MediaQuery.of(context).size.width / 3) /
+                                          150,
+                                  crossAxisCount: 3,
+                                  crossAxisSpacing: 12,
+                                  mainAxisSpacing: 2),
+                          itemCount:
+                              controller.availablePreferenceCards.length + 1,
+                          itemBuilder: (context, index) {
+                            if (index == 0) {
+                              return buildCustomCard(controller);
+                            }
+                            return buildPreferencesCard(controller, index - 1);
+                          }))),
+            ])),
+      ),
     );
   }
 
