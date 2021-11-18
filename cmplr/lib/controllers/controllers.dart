14bec83e644ba5788801160feaae2bb6683feb77
Mostafa wael <mostafa.w.k000@gmail.com>
2021-11-19 01:01:24 +0200

    signup_or_login

--- cmplr/lib/controllers/controllers.dart
+++ cmplr/lib/controllers/controllers.dart
@@ -2,5 +2,5 @@ export 'login_and_sign/signup_age_controller.dart';
 export 'login_and_sign/signup_preferences_controller.dart';
 export 'login_and_sign/signup_preferences_search_controller.dart';
 export 'login_and_sign/email_password_name_after_signup_controller.dart';
-export 'login_and_sign/intro_controller.dart';
+export 'login_and_sign/signup_or_login_controller.dart';
 export 'login_and_sign/forgot_password_controller.dart';
--- cmplr/lib/controllers/login_and_sign/signup_or_login_controller.dart
+++ cmplr/lib/controllers/login_and_sign/signup_or_login_controller.dart
@@ -1,9 +1,5 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import '../../views/login_and_sign/android_views.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class IntroController extends GetxController {
   Future<void> signIn() async {
