6ff544c89548141ca42b0e97dbabc722ce73fe1a
Mostafa wael <mostafa.w.k000@gmail.com>
2021-12-09 20:26:04 +0200

    simple fix

--- cmplr/lib/controllers/utilities/post_feed_controller.dart
+++ cmplr/lib/controllers/utilities/post_feed_controller.dart
@@ -1,34 +1,19 @@
+import '../../models/Utilities/posts/post_feed_model.dart';
+
 import '../../utilities/custom_widgets/custom_widgets.dart';
 
 import 'package:get/get.dart';
 
 class PostFeedController extends GetxController
     with SingleGetTickerProviderMixin {
+  final ModelPostsFeed _model = ModelPostsFeed();
+
   bool _isLoading = false;
   bool get isLoading => _isLoading;
   List<PostItem> posts = [];
 
   List<PostItem> getNewPosts() {
-    return [
-      const PostItem(
-        name: 'Mostafa',
-        profilePhoto: 'lib/utilities/assets/intro_screen/intro_4.jpg',
-        postData: 'lib/utilities/assets/intro_screen/intro_3.jpg',
-        numNotes: 200,
-        hashtags: [
-          'Gamadan',
-          'Roaan',
-          'Hiiii',
-        ],
-      ),
-      const PostItem(
-        name: 'Wael',
-        profilePhoto: 'lib/utilities/assets/intro_screen/intro_3.jpg',
-        postData: 'lib/utilities/assets/intro_screen/intro_4.jpg',
-        numNotes: 100,
-        hashtags: ['3azmaaaaaaaaaaaaaaaaaa', 'Hyhyhy', 'NNNAAANNNAAAA'],
-      )
-    ];
+    return _model.getNewPosts();
   }
 
   void initialPosts() async {
--- cmplr/lib/models/Utilities/posts/post_feed_model.dart
+++ cmplr/lib/models/Utilities/posts/post_feed_model.dart
@@ -8,16 +8,40 @@ import 'package:http/http.dart' as http;
 
 class ModelPostsFeed {
   // TODO: this function should return list of posts
-  Future<http.Response> getNewPosts(String postType) async {
-    final response = await CMPLRService.post(
-      PostURIs.posts,
-      {
-        'postType': postType,
-      },
-    );
-    // TODO: you should porse the JSON
-    final postMap = jsonDecode(response.body);
+//   Future<http.Response> getNewPosts(String postType) async {
+//     final response = await CMPLRService.post(
+//       PostURIs.posts,
+//       {
+//         'postType': postType,
+//       },
+//     );
+//     // TODO: you should porse the JSON
+//     final postMap = jsonDecode(response.body);
 
-    return postMap;
+//     return postMap;
+//   }
+// }
+
+  List<PostItem> getNewPosts() {
+    return [
+      const PostItem(
+        name: 'Mostafa',
+        profilePhoto: 'lib/utilities/assets/intro_screen/intro_4.jpg',
+        postData: 'lib/utilities/assets/intro_screen/intro_3.jpg',
+        numNotes: 200,
+        hashtags: [
+          'Gamadan',
+          'Roaan',
+          'Hiiii',
+        ],
+      ),
+      const PostItem(
+        name: 'Wael',
+        profilePhoto: 'lib/utilities/assets/intro_screen/intro_3.jpg',
+        postData: 'lib/utilities/assets/intro_screen/intro_4.jpg',
+        numNotes: 100,
+        hashtags: ['3azmaaaaaaaaaaaaaaaaaa', 'Hyhyhy', 'NNNAAANNNAAAA'],
+      )
+    ];
   }
 }
