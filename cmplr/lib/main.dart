df75126cb4d4daa6b528528126cef9adc3527ed4
Mostafa Elgendy <62804966+mostafa-elgendy22@users.noreply.github.com>
2021-12-11 01:31:50 +0200

    edit

--- cmplr/lib/main.dart
+++ cmplr/lib/main.dart
@@ -29,16 +29,13 @@ class CMPLR extends StatelessWidget {
     final themes = <ThemeData>[CMPLRTheme.trueBlue(), CMPLRTheme.darkTheme()];
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MasterPage(),
+      home: PersistentStorage.isLoggedIn
+          ? const MasterPage() /*const MasterPage()*/
+          : const SignupOrLoginScreen() /*SignupOrLoginScreen()*/, ,
       theme: themes[1],
       getPages: getLoginAndSignPages + getHomeScreenPages,
     );
   }
-  /*
-  PersistentStorage.isLoggedIn
-          ? const MasterPage() /*const MasterPage()*/
-          : const SignupOrLoginScreen() /*SignupOrLoginScreen()*/, 
-  */
 
   List<GetPage<dynamic>> get getHomeScreenPages {
     return [
