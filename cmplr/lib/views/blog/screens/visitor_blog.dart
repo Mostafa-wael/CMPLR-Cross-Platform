65eba0021ef6e512de635a2a11196475c4f3024e
yousefgamal295 <yousefgamal295@outlook.com>
2021-12-27 22:34:12 +0200

    Blog Integration

--- cmplr/lib/views/blog/screens/visitor_blog.dart
+++ cmplr/lib/views/blog/screens/visitor_blog.dart
@@ -1,8 +1,6 @@
-import '../../../utilities/custom_icons/custom_icons.dart';
-import '../../../utilities/custom_widgets/custom_widgets.dart';
+import '../../../routes.dart';
+
 import '../../../utilities/sizing/sizing.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 
 import '../../../controllers/utilities/post_feed_controller.dart';
 
@@ -12,31 +10,18 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
 class VisitorBlog extends StatelessWidget {
-  VisitorBlog(
-      {Key? key,
-      required this.postData,
-      required this.isLiked,
-      required this.name,
-      required this.profilePhoto,
-      required this.numNotes,
-      required this.hashtags})
-      : super(key: key);
+  VisitorBlog({Key? key, required this.blogId}) : super(key: key);
 
-  final String postData;
+  final String blogId;
   final double coverHight = 280.0;
   final double profileHeight = 144.0;
-  RxBool isLiked = false.obs;
-  final String name;
-  final String profilePhoto;
-  final int numNotes;
-  final List<dynamic> hashtags;
 
+  final blogController = Get.put(BlogController(), tag: 'BlogController');
   @override
   Widget build(BuildContext context) {
     final top = coverHight - profileHeight / 2;
     final bottom = profileHeight / 2;
     final _scrollController = ScrollController();
-    final blogController = Get.put(BlogController());
 
     Sizing.width = MediaQuery.of(context).size.width;
     Sizing.height = MediaQuery.of(context).size.height;
@@ -44,8 +29,12 @@ class VisitorBlog extends StatelessWidget {
     Sizing.blockSizeVertical = Sizing.height / 100;
     Sizing.setFontSize();
 
-    final postController = Get.put(PostFeedController());
+    final postController =
+        Get.put(PostFeedController(postFeedTypeFeed: Routes.homeTab));
     postController.updatePosts();
+
+    blogController.fetchBlogInfo(blogId);
+
     return Obx(() => Scaffold(
         extendBodyBehindAppBar: true,
         appBar: _visitorBlogAppBar(blogController),
@@ -70,14 +59,14 @@ class VisitorBlog extends StatelessWidget {
                   height: 10,
                 ),
                 // Profile Name
-                const Text(
-                  'Yousef Gamal',
-                  style: TextStyle(fontSize: 20),
+                Text(
+                  blogController.isBlogInfoLoading.value
+                      ? ''
+                      : blogController.blogInfo!.value.name!,
+                  style: const TextStyle(fontSize: 20),
                 ),
-                //postController.dataReloaded?
+
                 _postsWidget(context, postController)
-                //: const Center(
-                //child: CircularProgressIndicator(),)
               ],
             ),
           ),
@@ -125,9 +114,9 @@ class VisitorBlog extends StatelessWidget {
       elevation: 0,
       backgroundColor:
           blogController.scrolledDown.value ? Colors.black : Colors.transparent,
-      title: const Text(
-        'Yousef Gamal',
-        style: TextStyle(color: Colors.white),
+      title: Text(
+        blogController.blogInfo!.value.name!,
+        style: const TextStyle(color: Colors.white),
       ),
       leading: IconButton(
         onPressed: () {},
@@ -164,265 +153,19 @@ class VisitorBlog extends StatelessWidget {
   }
 
   Widget _buildCoverImage(BuildContext context) => Container(
-        color: Colors.grey,
-        child: Image.network(
-          'https://www.fcbarcelona.com/fcbarcelona/photo/2020/02/24/3f1215ed-07e8-47ef-b2c7-8a519f65b9cd/mini_UP3_20200105_FCB_VIS_View_1a_Empty.jpg',
-          width: double.infinity,
-          height: coverHight,
-          fit: BoxFit.cover,
-        ),
-      );
-  Widget _buildProfilePic(BuildContext context) => CircleAvatar(
-        backgroundColor: Colors.grey.shade800,
-        radius: profileHeight / 2,
-        backgroundImage: const NetworkImage(
-            'https://s.hs-data.com/bilder/spieler/gross/211506.jpg'),
-      );
-
-  Widget _postRetrive(BuildContext context) => Card(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        'https://s.hs-data.com/bilder/spieler/gross/211506.jpg'),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  const Text('Yousef Gamal')
-                ],
-              ),
-              const Text('Welcome to CMPLR Blog, it is my Blog'),
-            ],
-          ),
-        ),
-      );
-
-  Widget getUpperBar(BuildContext context) {
-    return ListTile(
-      contentPadding: const EdgeInsets.fromLTRB(8, 4, 8, 8),
-      leading: CircleAvatar(
-        backgroundImage: AssetImage(
-          '${postData}',
-        ),
-      ),
-      title: InkWell(
-        onTap: () {
-          print('Profile clicked');
-        },
-        child: Text(
-          '${name}',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Theme.of(context).primaryColor,
-          ),
-        ),
-      ),
-      trailing: IconButton(
-        icon: const Icon(Icons.more_horiz, color: Colors.grey),
-        onPressed: () {
-          //controller.openMoreOptions();
-          print('More Options clicked');
-        },
-      ),
-    );
-  }
-
-  Widget getPostData(BuildContext context) {
-    return FittedBox(
-      child: Image.asset(
-        '${profilePhoto}',
-        height: 170,
-        width: MediaQuery.of(context).size.width,
+      color: Colors.grey,
+      child: Image.network(
+        'https://www.fcbarcelona.com/fcbarcelona/photo/2020/02/24/3f1215ed-07e8-47ef-b2c7-8a519f65b9cd/mini_UP3_20200105_FCB_VIS_View_1a_Empty.jpg',
+        width: double.infinity,
+        height: coverHight,
         fit: BoxFit.cover,
-      ),
-      fit: BoxFit.fill,
-    );
-  }
+      ));
 
-  List<TextSpan> getHashtags(List<dynamic> hashtags) {
-    final hashtagsWidget = <TextSpan>[];
-    for (final hashtag in hashtags) {
-      hashtagsWidget.add(
-        TextSpan(
-            text: '#' + hashtag,
-            style: TextStyle(
-                color: Colors.grey.withOpacity(0.8),
-                fontSize: Sizing.fontSize * 5),
-            recognizer: TapGestureRecognizer()
-              ..onTap = () {
-                print(hashtag);
-              }),
-      );
-      hashtagsWidget.add(
-        const TextSpan(
-          text: ' ', // do nothing when space is clicked
-        ),
-      );
-    }
-    return hashtagsWidget;
-  }
-
-  Widget getHashtagsBar(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: RichText(
-        text: TextSpan(
-          children: getHashtags(hashtags),
-        ),
-      ),
-    );
-  }
-
-  Widget getBottomBar(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        TextButton(
-          style: TextButton.styleFrom(
-            textStyle: TextStyle(fontSize: Sizing.fontSize * 3.8),
-          ),
-          onPressed: () {
-            //controller.openNotes(numNotes);
-            print('Notes clicked');
-          },
-          child: Text('${numNotes} notes',
-              style: const TextStyle(
-                  color: Colors.grey, fontWeight: FontWeight.bold)),
-        ),
-        Row(
-          children: [
-            IconButton(
-              icon: Icon(Icons.share, color: Theme.of(context).primaryColor),
-              onPressed: () {
-                showModalBottomSheet(
-                  isScrollControlled: true,
-                  context: context,
-                  constraints: BoxConstraints(
-                    maxHeight: Sizing.blockSizeVertical * 90,
-                  ),
-                  shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(Sizing.blockSize * 5)),
-                  builder: (BuildContext context) {
-                    return Padding(
-                      padding: EdgeInsets.fromLTRB(
-                          Sizing.blockSize * 4, 0, Sizing.blockSize * 4, 0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          SizedBox(height: Sizing.blockSizeVertical * 3),
-                          Container(
-                            width: Sizing.blockSize * 12,
-                            height: Sizing.blockSize * 1,
-                            //TODO: Link this to theme
-                            decoration: BoxDecoration(
-                                color: Colors.grey,
-                                borderRadius: BorderRadius.all(
-                                    Radius.circular(Sizing.blockSize))),
-                          ),
-                          SizedBox(height: Sizing.blockSizeVertical * 3),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              UserNameAvatar(
-                                  profilePhoto,
-                                  name,
-                                  TextStyle(
-                                    fontSize: Sizing.fontSize * 5,
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                            ],
-                          ),
-                          SizedBox(height: Sizing.blockSizeVertical * 4),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              InkResponse(
-                                child: Column(
-                                  children: [
-                                    const Icon(Icons.copy),
-                                    const Text(
-                                      'Copy',
-                                    )
-                                  ],
-                                ),
-                                onTap: () {},
-                              ),
-                              InkResponse(
-                                child: Column(
-                                  children: [
-                                    const Icon(Icons.share),
-                                    const Text(
-                                      'Other',
-                                    )
-                                  ],
-                                ),
-                                onTap: () {
-                                  //controller.share(context, 'Test1');
-                                },
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: Sizing.blockSizeVertical * 4,
-                          ),
-                          Container(
-                              width: Sizing.blockSize * 92,
-                              height: Sizing.blockSize * 0.25,
-                              color: Colors.white),
-                          SizedBox(
-                            height: Sizing.blockSizeVertical * 4,
-                          ),
-                          TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      Sizing.blockSize * 2),
-                                ),
-                                filled: true,
-                                hintStyle: const TextStyle(color: Colors.white),
-                                hintText: 'Type in your text',
-                                fillColor: Colors.white70),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-            IconButton(
-              icon: Icon(CustomIcons.comment,
-                  color: Theme.of(context).primaryColor),
-              onPressed: () {
-                //controller.openNotes(numNotes);
-                print('Notes clicked');
-              },
-            ),
-            IconButton(
-              icon: Icon(CustomIcons.reblog,
-                  color: Theme.of(context).primaryColor),
-              onPressed: () {
-                print('reblog clicked');
-              },
-            ),
-            Obx(() => IconButton(
-                  icon: Icon(
-                      isLiked.value ? Icons.favorite : CupertinoIcons.heart,
-                      color: Theme.of(context).primaryColor),
-                  onPressed: () {
-                    isLiked.value = !isLiked.value;
-                  },
-                )),
-          ],
-        )
-      ],
-    );
-  }
+  Widget _buildProfilePic(BuildContext context) => CircleAvatar(
+      backgroundColor: Colors.grey.shade800,
+      radius: profileHeight / 2,
+      backgroundImage: blogController.isBlogInfoLoading.value
+          ? const NetworkImage(
+              'https://s.hs-data.com/bilder/spieler/gross/211506.jpg')
+          : NetworkImage(blogController.blogInfo!.value.avatar!));
 }
