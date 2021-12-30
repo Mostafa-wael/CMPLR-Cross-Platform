da25fd2a49a092e5a356d04f937a133fcd6d22ea
Mostafa wael <mostafa.w.k000@gmail.com>
2021-12-30 13:03:28 +0200

    set mock flag to false

--- cmplr/lib/flags.dart
+++ cmplr/lib/flags.dart
@@ -1,6 +1,6 @@
 /// Holds global flags used for mocking and erasing persisent storage.
 class Flags {
-  static bool mock = true;
+  static bool mock = false;
 
   // Clears persistent data on app launch.
   // WARNING: MAKE SURE YOU UNDERSTAND WHAT'S GOING TO BE ERASED
